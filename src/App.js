ac5b1ea1db71f7eb3adeb07d355d9671ea8bad40
cliffmax85 <cliffmax85@gmail.com>
2022-02-03 13:33:12 -0800

    added pokemon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react';
 import { fetchBands } from './services/fetch-utils';
+import { fetchPokemon } from './services/fetch-utils';
+import PokemonList from './Pokemon/PokemonList';
 import BandList from './Bands/BandList';
 import './App.css';
 
 function App() {
   const [bands, setBands] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
+
 
   async function fetchBandsData() {
     const data = await fetchBands();
@@ -12,13 +16,21 @@ function App() {
     setBands(data);
   }
 
+  async function fetchPokemonData() {
+    const data = await fetchPokemon();
+
+    setPokemon(data);
+  }
+
   useEffect(() => {
     fetchBandsData();
+    fetchPokemonData();
   }, []);
 
   return (
     <div className="App">
       <BandList bands={bands} />
+      <PokemonList pokemon={pokemon} />
     </div>
   );
 }
